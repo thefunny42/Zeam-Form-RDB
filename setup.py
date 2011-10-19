af0882edec44d14a93e62ab2abff8e852c8ef977
Sylvain Viollon <thefunny@gmail.com>
2011-10-19 15:12:32 +0200

    Skeleton

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.3dev'
+
+tests_require = [
+    'zope.app.wsgi',
+    'zope.testing',
+    'zeam.form.base [test]',
+    ]
+
+setup(name='zeam.form.composed',
+      version=version,
+      description="Extra SQLAlchemy support for zeam.form",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='zeam form rdb',
+      author='Sylvain Viollon',
+      author_email='thefunny@gmail.com',
+      url='http://pypi.python.org/pypi/zeam.form.rdb',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['zeam', 'zeam.form'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'zeam.form.base >= 1.0',
+        ],
+      tests_require = tests_require,
+      extras_require = {'test': tests_require},
+      )
